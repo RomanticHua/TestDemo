1f52a055839eee6b32958231db83e1113d3bd30a
10616 <1061625903@qq.com>
2018-10-14 20:11:01 +0800

    主分支修改

--- app/src/main/java/com/example/a10616/testdemo/MainActivity.java
+++ app/src/main/java/com/example/a10616/testdemo/MainActivity.java
@@ -116,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         bind.unbind();
+        Log.e(TAG, "onDestroy: 主分支代码修改");
     }
 
 }
