7f51a123acc52cd45af731d08755978ace4dc942
10616 <1061625903@qq.com>
2018-11-04 01:21:15 +0800

    44444

--- app/src/main/java/com/example/a10616/testdemo/Delete.java
+++ app/src/main/java/com/example/a10616/testdemo/Delete.java
@@ -11,5 +11,15 @@ public class Delete {
         Log.i(TAG, "apply: 开发到一半了,需要切换到其他分支...,现在进行暂存操作.");
 
         Log.i(TAG, "apply: 开发到3/4,又需要切换到其他分支...");
+
+        Log.i(TAG, "apply: soft");
+
+        Log.i(TAG, "apply: 1111");
+
+        Log.i(TAG, "apply: 222");
+
+        Log.i(TAG, "apply: 333");
+
+        Log.i(TAG, "apply: 444");
     }
 }
