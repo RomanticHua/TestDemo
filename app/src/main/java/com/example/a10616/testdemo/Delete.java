73525abcefbcbef91285851d5e8b7a04dbef2829
10616 <1061625903@qq.com>
2018-11-03 23:40:14 +0800

    vv

--- app/src/main/java/com/example/a10616/testdemo/Delete.java
+++ app/src/main/java/com/example/a10616/testdemo/Delete.java
@@ -1,4 +1,11 @@
 package com.example.a10616.testdemo;
 
+import android.util.Log;
+
 public class Delete {
+    public static final String TAG = Delete.class.getSimpleName();
+
+    public void apply() {
+        Log.i(TAG, "apply: ");
+    }
 }
