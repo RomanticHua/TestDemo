d047f8cea03a4ecf7da3fa3d7e22d58b321e37ef
10616 <1061625903@qq.com>
2018-11-03 23:58:16 +0800

    主分支开发完毕

--- app/src/main/java/com/example/a10616/testdemo/Delete.java
+++ app/src/main/java/com/example/a10616/testdemo/Delete.java
@@ -7,5 +7,9 @@ public class Delete {
 
     public void apply() {
         Log.i(TAG, "apply: ");
+
+        Log.i(TAG, "apply: 开发到一半了,需要切换到其他分支...,现在进行暂存操作.");
+
+        Log.i(TAG, "apply: 开发到3/4,又需要切换到其他分支...");
     }
 }
