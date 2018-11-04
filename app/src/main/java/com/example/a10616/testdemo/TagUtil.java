db7c6f4c4c2bed942c7e8377d0944a116fa66cc5
10616 <1061625903@qq.com>
2018-11-04 18:13:21 +0800

    v1.0.0

---
+++ app/src/main/java/com/example/a10616/testdemo/TagUtil.java
@@ -0,0 +1,12 @@
+package com.example.a10616.testdemo;
+
+import android.util.Log;
+
+public class TagUtil {
+
+    public static final String TAG = TagUtil.class.getSimpleName();
+
+    public void tag() {
+        Log.i(TAG, "tag: 正式发版1.0.0");
+    }
+}
